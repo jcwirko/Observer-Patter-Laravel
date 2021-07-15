e7d4c3c9cf2a5fd30af759d058f1053c5893319f
juanblariza <juanblariza@gmail.com>
2021-07-15 08:51:53 -0300

    Setup

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
-
-Route::post('payments/add', [PaymentController::class, 'add']);
